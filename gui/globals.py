ea70c6e92161cbf194c3bdbba00a1f3bd9fc53a7
Petur Helgi Einarsson <phe1@hi.is>
2016-07-10 16:59:41 -0700

    Added GUI files

---
+++ gui/globals.py
@@ -0,0 +1,5 @@
+global workspace
+global current_project 
+
+#workspace='gui/workspace'
+#current_project= 'AAV-project'
---
+++ gui/methods.py
@@ -0,0 +1,22 @@
+methods = {
+	'Perfect match': 
+		{
+		'Perfect param 1': 'Checkbutton',
+		'Perfect param 2': 'Checkbutton',
+		'Perfect param 3': 'Entry'
+		},
+	'BowTie': 
+		{
+		'BowTie param 1': 'Checkbutton',
+		'BowTie param 2': 'Checkbutton',
+		'Number of mistakes': 'Entry',
+		'Allow insertions/deletions':'Checkbutton'
+		},
+	'Mosaik': 
+		{
+		'Mosaik param 1': 'Checkbutton',
+		'Mosaik param 2': 'Checkbutton',
+		'Mosaik param 3': 'Entry',
+		'Mosaik param 4': 'Checkbutton'
+		}
+	}
