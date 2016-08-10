4a2f18d1c02423ddd25f7eaffc7a155653e889f6
David Brown <dibidave@gmail.com>
2016-08-09 21:53:06 -0700

    Threaded aligner, output progress to GUI

--- bin/launch.py
+++ bin/launch.py
@@ -3,11 +3,18 @@ from tkFileDialog import askopenfilename
 import ttk
 import tkFont
 from gui.main import Application
+from workspace import Workspace as ws
+import sys
 
 root = Tk()
 root.attributes('-zoomed', True)
 
 root.wm_title('CLOVERpuff')
+
+if len(sys.argv) > 1:
+    ws.set_workspace_path(sys.argv[1])
+else:
+    ws.set_workspace_path('.')
 
 app = Application(master=root)
 app.mainloop()
